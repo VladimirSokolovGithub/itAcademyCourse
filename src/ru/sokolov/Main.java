9c8878a0f849bbaf03b16e757d87c05a2906e320
house-37 <house-37@mail.ru>
2021-04-05 14:44:09 +0300

    Commit first lesson

---
+++ src/ru/sokolov/Main.java
@@ -0,0 +1,69 @@
+package ru.sokolov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// не возвращающие методы
+        printMsg();
+        String str = "Hello";
+        printMessage(str);
+        printMessage("Hello my brother");
+
+    // возвращающие методы
+
+        int res = calcTwoNumber(2,3);
+        System.out.println(res);
+
+    // другие примеры
+    String s = null;
+    int a = 10;
+
+    // Двойной амперсанд && проверяет если первое условие истинно, значит второе уже не проверяется
+    if(a < 5 && s.equals("a")){
+        System.out.println("Ho ho ho");
+    } else {
+        System.out.println("end");
+    }
+
+    // Одинарный амперсанд & проверяет оба условия даже если первое условие истинно, в нашем случае второе условие false
+       /*
+       if(a < 5 & s.equals("a")){
+            System.out.println("Ho ho ho");
+        } else {
+            System.out.println("end");
+        }
+        */
+
+        //Приведение типов данных
+        //бывает явное и неявное
+
+
+        //Тернарный оператор
+        int abc = 100;
+
+        System.out.println(abc == 100 ? "abc = 100" : "abc !=100");
+
+        if(abc ==10){
+            System.out.println("abc = 10");
+        } else {
+            System.out.println("abc != 10");
+        }
+
+
+    }
+    // не возвращающие методы
+    public static void printMsg() {
+        System.out.println("Hello world!");
+    }
+
+    public static void printMessage(String msg) {
+        System.out.println(msg);
+    }
+
+    // возвращающие методы
+    public static int calcTwoNumber(int a, int b) {
+        int res = a + b;
+        return res;
+    }
+
+}
